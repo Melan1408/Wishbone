6a6643d74d6c69a1e85ef646a5351edd8c856a59
Andrey <andreymelnik9517@gmail.com>
2022-08-23 23:24:03 +0300

    Owl carousel was added

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots: true,
+        dotsEach: true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:3
+            }
+        }
+    });
+});
